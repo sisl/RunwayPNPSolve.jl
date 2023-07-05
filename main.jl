0ab9a48be35504b0e5f3e0b3a51835cbdd0e315b
Romeo Valentin <romeo.valentin.int@airbus-sv.com>
2023-07-05 11:15:09 -0700

    Add a bunch of comments.

--- main.jl
+++ main.jl
@@ -20,8 +20,8 @@ z-axis up.
 
 For now we assume that all runway points are in the x-y plane.
 """
-# Pos3 = SVector{3, Float64}
-# Rot3 = SVector{3, Float64}
+
+# Point2/3f already exists, also define for double precision
 Point2d = Point2{Float64}
 Vec2d = Vec2{Float64}
 Point3d = Point3{Float64}
@@ -43,10 +43,12 @@ slidergrid =  SliderGrid(fig[2, 1],
 σ = lift(slidergrid.sliders[1].value) do x; exp(x) end
 rhs_grid = GridLayout(fig[1, 2]; tellheight=false)
 toggle_grid = GridLayout(rhs_grid[1, 1])
+# Set up noise toggles, scenario menu, num pose estimates
 toggles = [Toggle(toggle_grid[i, 2]; active=true) for i in 1:4]
-toggle_labels = let labels = ["Front left", "Front right", "Back left", "Back right"]
+toggle_labels = let labels = ["Noise front left:", "Noise front right:", "Noise back left:", "Noise back right:"]
     [Label(toggle_grid[i, 1], labels[i]) for i in 1:4]
 end
+## Set up scenario, which affects cam position and therefore all the projections
 Label(toggle_grid[5, 1], "Scenario:")
 scenario_menu = Menu(toggle_grid[5, 2]; options=["near (10m)", "mid (100m)", "far (500m)"], default="mid (100m)")
 #
@@ -61,7 +63,7 @@ projected_points = lift(cam_transform) do cam_transform; map(cam_transform, runw
 projected_points_global = lift(Cam_translation, projected_points) do Cam_translation, projected_points
     map(Cam_translation ∘ AffineMap(I(3)[:, 1:2], Float64[0;0;1]), projected_points)
 end
-#
+## plot points projected onto 2D camera plane
 projected_points_rect = lift(projected_points) do projected_points
     pts = map(p->typeof(p)(-p[2], -p[1]),
               projected_points) |> collect
@@ -71,19 +73,18 @@ projected_points_2d = [lift(projected_points_rect) do rect; rect[i] end
                        for i in 1:4]
 cam_view_ax = Axis(rhs_grid[2, 1], width=500, aspect=DataAspect(), limits=(-1,1,-1,1)./8)
 cam_view = lines!(cam_view_ax, projected_points_rect)
+# plot 1std of Gaussian noise
 meshscatter!(cam_view_ax, projected_points_2d[1], marker=Makie.Circle(Point2d(0,0), 1.0), markersize=σ)
 meshscatter!(cam_view_ax, projected_points_2d[2], marker=Makie.Circle(Point2d(0,0), 1.0), markersize=σ)
 meshscatter!(cam_view_ax, projected_points_2d[3], marker=Makie.Circle(Point2d(0,0), 1.0), markersize=σ)
 meshscatter!(cam_view_ax, projected_points_2d[4], marker=Makie.Circle(Point2d(0,0), 1.0), markersize=σ)
-#
-# rhs_grid[1, 1] = grid!(hcat(toggles, toggle_labels); tellheight=false, tellwidth=true)
-#
-#
+## Set up camera
 cam3d!(scene; near=0.01, far=1e9, rotation_center=:eyeposition, cad=true, zoom_shift_lookat=false,
        mouse_rotationspeed = 5f-1,
        mouse_translationspeed = 0.1f0,
        mouse_zoomspeed = 5f-1,
        )
+## Draw runway and coordinate system
 lines!(scene, runway_corners[[1, 2, 4, 3, 1]])
 # arrows!(scene, [Point3f(C_t_true), ], [Vec3f([1., 0, 0]), ]; normalize=true, lengthscale=0.5)
 arrows!(scene,
@@ -96,17 +97,22 @@ arrows!(scene,  # larger coordinate system
         Vec3f[[1,0,0,],[0,1,0],[0,0,1]]*5;
         arrowsize=Vec3f(2, 2, 3)
         )
+# Plot runway surface
 surface!(scene, getindex.(runway_corners, 1),
                 getindex.(runway_corners, 2),
                 getindex.(runway_corners, 3))
+# Draw lines from corners to camera
 corner_lines = [lift(C_t_true) do C_t_true
                     [p, C_t_true]
                     end for p in runway_corners]
 for l in corner_lines
     lines!(scene, l)
 end
+# Draw Projected points
 scatter!(scene, projected_points_global)
+# Set cam position
 update_cam!(scene.scene, Array(C_t_true[]).-[20.,0,0], Float32[0, 0, 0])
+# Compute pose estimates
 perturbation_mask = lift(toggles[1].active, toggles[2].active, toggles[3].active, toggles[4].active) do a, b, c, d;
     Int[a;b;c;d]
 end
@@ -118,6 +124,7 @@ perturbed_locations = lift(projected_points, σ, perturbation_mask, num_pose_est
     pts = Point3d.([perturb_x1(projected_points, σ; mask=mask) for _ in 1:num_pose_est])
     # filter(p -> (p[2] ∈ 0±30) && (p[3] ∈ 0..50) && (p[1] ∈ -150..0),
     #        pts) |> collect
+    pts
 end
 scatter!(scene, perturbed_locations; color=:red)
 fig
