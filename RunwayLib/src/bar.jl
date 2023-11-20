8a67f42650e45fac194a8ea72c335d7a87d82f3b
Romeo Valentin <romeov@stanford.edu>
2023-11-20 15:55:53 -0800

    Move to `SimpleNonlinearSolve` since it turns out
    the "simple" implementation of NewtonRaphson is enough for us. May
    revert later.

--- RunwayLib/src/bar.jl
+++ RunwayLib/src/bar.jl
@@ -1,6 +1,12 @@
-using Optimization
-using OptimizationOptimJL
-using NonlinearSolve
+# using Optimization
+# using OptimizationOptimJL
+# run ] dev Unitful GeodesyXYZExt .
+# run ] add SimpleNonlinearSolve ForwardDiff CoordinateTransformations Rotations StaticArraysCore ForwardDiff
+using Rotations, CoordinateTransformations, NonlinearSolve, Unitful, Unitful.DefaultSymbols, GeodesyXYZExt, RunwayLib, StaticArraysCore
+using SimpleNonlinearSolve
+import SciMLBase: ReturnCode
+import RunwayLib: CamTransform, project
+import LinearAlgebra: norm
 function solve_once(pt2_x::T) where T
     pose = AffineMap(RotY(0.), XYZ(-100.0m, 0.0m, 0.0m))
     pts = [XYZ(1.0m, 2.0m, 3.0m) + XYZ(pt2_x*m, 0.0m, 0.0m),
@@ -22,37 +28,27 @@ function solve_once(pt2_x::T) where T
 end
 # ForwardDiff.derivative(solve_once, 0.0)
 
-
-function solve_once_nlls(pt2_y::T, solver=LevenbergMarquardt()) where T
-    pose = AffineMap(RotY(0.), XYZ(-6000.0m, 0.0m, 125.0m))
+function solve_once_nlls(pt2_y::T, solver=SimpleNewtonRaphson()) where T
+    cam_rot = RotY(0.); cam_loc = XYZ(-6000.0m, 0.0m, 125.0m)
+    pose = CamTransform(cam_rot, cam_loc)
     pts = [XYZ(1.0m, 25.0m, 3.0m),
            XYZ(1.0m, -25.0m, 3.0m)];
     projections = project.([pose], pts);
-    seed!(1)
+    # seed!(1)
     # measurements = projections .+ 0 .*[3*randn(2)pxl for _ in eachindex(projections)]
     measurements = projections .+ [ImgProj(0.0pxl, 0.0pxl), ImgProj(pt2_y*pxl, 0.0pxl)]
-    loss(loc) = begin
-        simulated_projections = project.([AffineMap(pose.linear, XYZ(loc*m))], pts)
-        res = map((lhs, rhs)::Tuple->norm(lhs - rhs), zip(measurements, simulated_projections)) |> collect |> SVector{length(pts)}
+    loss(loc, (;cam_rot, pts, measurements)) = begin
+        simulated_projections = project.([CamTransform(cam_rot, XYZ(loc*m))], pts)
+        res = map((lhs, rhs)::Tuple->norm(lhs - rhs),
+                  zip(measurements, simulated_projections))
         ustrip.(pxl, res)
     end
-    loss(loc, p) = loss(loc);
-    # maybe make this in-place, but there seem to be dimensionality problems...
-    loss!(buf, loc) = begin
-        simulated_projections = project.([AffineMap(pose.linear, XYZ(loc*m))], pts)
-        # THIS DOESN'T WORK FOR MORE POINTS. THERE MUST BE A BUG (?)
-        buf[1:length(pts)] .= map((lhs, rhs)::Tuple->norm(lhs - rhs),
-                                  zip(measurements, simulated_projections)) |> collect .|> x->ustrip(pxl, x)
-    end
-    loss!(buf, loc, p) = begin
-        loss!(buf, loc)
-    end
+    # loss(loc, p) = loss(loc)
 
-    prob = NonlinearLeastSquaresProblem(loss, MVector{3, T}(-3000. * one(T), 0. * one(T), 0. * one(T)))
-    # prob = NonlinearLeastSquaresProblem{true}(loss!, MVector{3, T}(-50. * one(T), 0. * one(T), 0. *one(T)))
-    # prob = NonlinearLeastSquaresProblem(loss, [-3000. * one(T), 0. * one(T), 100. * one(T)])
+    initial_guess = MVector{3, T}([-3000., 0., 0.].*one(T))  # T may be, for example, Dual{Float64}
+    prob = NonlinearLeastSquaresProblem(loss, initial_guess, (; cam_rot, pts, measurements))
     res = solve(prob, solver; maxiters=1_000_000)
-    @assert res.retcode == Optimization.ReturnCode.Success
+    @assert res.retcode == ReturnCode.Success
     res.u
 end
 # This works!!
