8a498ad4db37806c9589b86ec17aacd3b4e8e7d9
Romeo Valentin <romeov@stanford.edu>
2023-11-20 17:07:01 -0800

    Start overhauling projection interface slightly

--- RunwayLib/src/projections.jl
+++ RunwayLib/src/projections.jl
@@ -15,8 +15,10 @@
 struct ProjectionMap{N} end
 getaxis(::ProjectionMap{N}) where {N} = N
 function projectionmap_(::Val{N}, svec::StaticVector{3,T}) where {N, T}
-    idx = filter(!=(N), first(axes(svec)))
+    idx = filter(!=(N), axes(svec, 1))
     proj = svec[idx] * inv(svec[N])
+    #or, (about 50% faster)
+    # proj = svec[vcat(1:(N-1), (N+1):end)] * inv(svec[N])
     return proj
 end
 function (pmap::ProjectionMap)(svec::StaticVector{3,T}) where {T}
@@ -27,17 +29,73 @@ cameramap(::Val{N}) where {N} = ProjectionMap{N}()
 cameramap(::Val{N}, scale::Number) where {N} =
     LinearMap(UniformScaling(scale)) ∘ ProjectionMap{N}()
 
+CamTransform = AffineMap
 
-function make_projection_fn(cam_pose::AffineMap{<:Rotation{3,Float64},<:XYZ})
+function project(cam_pose::CamTransform{<:Rotation{3}, <:XYZ{<:WithUnits(m)}},
+                 world_point::XYZ{<:WithUnits(m)})::ImgProj
     scale = let focal_length = 25mm, pixel_size = 0.00345mm / 1pxl
         focal_length / pixel_size
     end
-    cam_transform = cameramap(Val(1), scale) ∘ inv(cam_pose)
-    function proj(p::XYZ{T})::ImgProj where T
-        (ImgProj ∘ cam_transform)(p)
-    end
-    function proj(rc::RunwayCorners(XYZ{Meters}))::RunwayCorners(ImgProj{Pixels})
-        RunwayCorners(ImgProj)(proj.(values(rc)))
-    end
-    return proj
+    cam_transform = cameramap(Val(1), scale) ∘ inv(cam_pose)  # first axis aligned with direction of view
+    orient_coord_sys =  LinearMap([-1 0 ; 0 1])  # flip x, leave y
+    transform = (ImgProj ∘ orient_coord_sys ∘ cam_transform)
+    transform(world_point)
 end
+
+# # get derivatives of pose
+# derivative((@_ ustrip(pxl, norm(__)) ∘ project(pose, pt + XYZ(__*m, 0.0m, 0.0m))), 1.0)
+# gradient((@_ ustrip(pxl, norm(__)) ∘ project(pose, pt + XYZ(__*m))), zeros(3))
+# jacobian((@_ ustrip.(pxl, __) ∘ project(pose, pt + XYZ(__*m))), zeros(3))
+#
+# # get derivatives of orientation
+# let new_pose(θ) = AffineMap(RotZ(θ)*pose.linear, pose.translation)
+#     derivative((@_ ustrip(pxl, norm(__)) ∘ project(new_pose(__), pt)), 0.)
+# end
+# let new_pose(θs) = AffineMap(RotYZ(θs...)*pose.linear, pose.translation)
+#     gradient((@_ ustrip(pxl, norm(__)) ∘ project(new_pose(__), pt)), zeros(2))
+# end
+# let new_pose(θs) = AffineMap(RotYZ(θs...)*pose.linear, pose.translation)
+#   jacobian((@_ ustrip.(pxl, __) ∘ project(new_pose(__), pt)), zeros(2))
+# end
+#
+#
+# pose = AffineMap(RotY(0.), XYZ(-100.0m, 0.0m, 0.0m))
+# pts = [XYZ(1.0m, 2.0m, 3.0m), XYZ(1.0m, -2.0m, 3.0m)];
+# projections = project.([pose], pts);
+# measurements = projections .+ [3*randn(2)pxl for _ in eachindex(projections)]
+# loss(loc) = begin
+#   simulated_projections = project.([AffineMap(pose.linear, XYZ(loc*m))], pts)
+#   res = sum((lhs, rhs)::Tuple->norm(lhs - rhs), zip(measurements, simulated_projections))
+#   ustrip(pxl, res)
+# end
+# loss(loc, p) = loss(loc);
+#
+# using Optimization
+# using OptimizationOptimJL
+# prob = OptimizationProblem(OptimizationFunction(loss, Optimization.AutoForwardDiff()), MVector(-10., 0., 0.))
+# solve(prob, NewtonTrustRegion())
+
+
+# using Optimization
+# using OptimizationOptimJL
+# using NonlinearSolve
+function solve_once(pt2_x::T) where T
+    pose = AffineMap(RotY(0.), XYZ(-100.0m, 0.0m, 0.0m))
+    pts = [XYZ(1.0m, 2.0m, 3.0m) + XYZ(pt2_x*m, 0.0m, 0.0m),
+           XYZ(1.0m, -2.0m, 3.0m)];
+    projections = project.([pose], pts);
+    measurements = projections .+ [3*randn(2)pxl for _ in eachindex(projections)]
+    loss(loc) = begin
+        simulated_projections = project.([AffineMap(pose.linear, XYZ(loc*m))], pts)
+        res = sum((lhs, rhs)::Tuple->norm(lhs - rhs), zip(measurements, simulated_projections))
+        ustrip(pxl, res)
+    end
+    loss(loc, p) = loss(loc);
+
+    func = OptimizationFunction(loss, Optimization.AutoForwardDiff())
+    # prob = NonlinearLeastSquaresProblem(func, MVector{3, T}(-90. * one(T), 0. * one(T), 0. * one(T)))
+    prob = OptimizationProblem(func, MVector{3, T}(-90. * one(T), 0. * one(T), 0. * one(T)))
+    # solve(prob, LevenbergMarquardt())
+    solve(prob, ConjugateGradient(), reltol=1e-14, allow_f_increases=true, maxiters=10_000)
+end
+# ForwardDiff.derivative(solve_once, 0.0)
