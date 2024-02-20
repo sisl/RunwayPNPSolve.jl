7c4cc66294daece03262555f8d7d6be9139d4c0b
Sydney Katz <smkatz@stanford.edu>
2024-02-20 10:04:34 -0800

    better conditioning for optimizer with edges

--- LsqPnP/src/pnp.jl
+++ LsqPnP/src/pnp.jl
@@ -19,6 +19,24 @@ function pnp(world_pts::AbstractVector{<:XYZ{<:WithUnits(m)}},
     isempty(components) && return initial_guess
     N = length(world_pts); @assert N == length(measurements)
 
+    loss_no_α(loc, (; cam_rot, world_pts, measurements, measured_lines, runway_pts)) = begin
+        simulated_projections = project.([CamTransform(cam_rot, XYZ(loc*m))], world_pts)
+        results = []
+        # process points
+        if (:x in components || :y in components)
+            inv_weights = cholesky(measurement_covariance).U'
+            res_pts = let measurements_ = SVector(flatten(measurements)...),
+                        simulated_projections_ = SVector(flatten(simulated_projections)...)
+                res = measurements_ .- simulated_projections_
+                inv_weights \ ustrip.(pxl, res)
+            end
+            append!(results, res_pts)
+        end
+
+        T = promote_type(eltype.(results)...)
+        return reduce(vcat, results; init=T[])
+    end
+
     loss(loc, (; cam_rot, world_pts, measurements, measured_lines, runway_pts)) = begin
         simulated_projections = project.([CamTransform(cam_rot, XYZ(loc*m))], world_pts)
         results = []
@@ -56,12 +74,23 @@ function pnp(world_pts::AbstractVector{<:XYZ{<:WithUnits(m)}},
         return reduce(vcat, results; init=T[])
     end
 
+    # Run first without edges to get good initial guess
     initial_guess = MVector{3}(ustrip.(m, initial_guess))
     ps = (; cam_rot, world_pts, measurements, measured_lines, runway_pts)
-    prob = NonlinearLeastSquaresProblem(loss, initial_guess, ps)
+    prob = NonlinearLeastSquaresProblem(loss_no_α, initial_guess, ps)
     res = solve(prob, solver; maxiters=100_000, abstol=1e-5)
     @assert res.retcode == ReturnCode.Success res
-    XYZ(res.u*m)
+    if !(:α in components)
+        return XYZ(res.u*m)
+    end
+
+    # Run with edges with good initial guess
+    new_initial_guess = XYZ(res.u*m)
+    new_initial_guess = MVector{3}(ustrip.(m, new_initial_guess))
+    prob = NonlinearLeastSquaresProblem(loss, new_initial_guess, ps)
+    res = solve(prob, solver; maxiters=100_000, abstol=1e-5)
+    @assert res.retcode == ReturnCode.Success res
+    return XYZ(res.u*m)
 end
 
 function pnp(world_pts::AbstractVector{<:XYZ{<:WithUnits(m)}},
@@ -78,6 +107,26 @@ function pnp(world_pts::AbstractVector{<:XYZ{<:WithUnits(m)}},
     )
     isempty(components) && return initial_guess
     N = length(world_pts); @assert N == length(measurements)
+
+    loss_no_α(pose, (; world_pts, measurements, measured_lines, runway_pts)) = begin
+        loc = pose[1:3]
+        cam_rot = RotXYZ(roll=pose[4], pitch=pose[5], yaw=pose[6])
+        simulated_projections = project.([CamTransform(cam_rot, XYZ(loc*m))], world_pts)
+        results = []
+        # process points
+        if (:x in components || :y in components)
+            inv_weights = cholesky(measurement_covariance).U'
+            res_pts = let measurements_ = SVector(flatten(measurements)...),
+                        simulated_projections_ = SVector(flatten(simulated_projections)...)
+                res = measurements_ .- simulated_projections_
+                inv_weights \ ustrip.(pxl, res)
+            end
+            append!(results, res_pts)
+        end
+
+        T = promote_type(eltype.(results)...)
+        return reduce(vcat, results; init=T[])
+    end
 
     loss(pose, (; world_pts, measurements, measured_lines, runway_pts)) = begin
         loc = pose[1:3]
@@ -118,9 +167,10 @@ function pnp(world_pts::AbstractVector{<:XYZ{<:WithUnits(m)}},
         return reduce(vcat, results; init=T[])
     end
 
+    # Run first without edges to get good initial guess
     initial_guess = MVector{6}(vcat(ustrip.(m, initial_guess_loc), initial_guess_rot))
     ps = (; world_pts, measurements, measured_lines, runway_pts)
-    prob = NonlinearLeastSquaresProblem(loss, initial_guess, ps)
+    prob = NonlinearLeastSquaresProblem(loss_no_α, initial_guess, ps)
     res = solve(prob, solver; maxiters=100_000, abstol=1e-5)
     @assert res.retcode == ReturnCode.Success res
     # @info loss(copy(res.u), ps)
@@ -128,7 +178,18 @@ function pnp(world_pts::AbstractVector{<:XYZ{<:WithUnits(m)}},
     loc = XYZ(res.u[1:3]*m)
     rot = res.u[4:6]
     # @info loss(vcat(ustrip.(m, loc), rot), ps)
-    loc, rot
+    if !(:α in components)
+        return loc, rot
+    end
+
+    # Run with edges with good initial guess
+    new_initial_guess = vcat(ustrip.(m, loc), rot)
+    prob = NonlinearLeastSquaresProblem(loss, new_initial_guess, ps)
+    res = solve(prob, solver; maxiters=100_000, abstol=1e-5)
+    @assert res.retcode == ReturnCode.Success res
+    loc = XYZ(res.u[1:3]*m)
+    rot = res.u[4:6]
+    return loc, rot
 end
 
 project_angle_onto_unit_circle(angle::AngularQuantity) = Point2(cos(angle), sin(angle))
