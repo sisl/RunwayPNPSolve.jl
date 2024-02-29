27e7dc064198c3e903280911f3892be2956160f1
Sydney Katz <smkatz@stanford.edu>
2024-02-29 10:46:44 -0800

    combine everything into a single pnp function

--- LsqPnP/src/pnp.jl
+++ LsqPnP/src/pnp.jl
@@ -6,38 +6,23 @@ import NonlinearSolve
 
 function pnp(world_pts::AbstractVector{<:XYZ{<:WithUnits(m)}},
              measurements::AbstractVector{<:ImgProj{<:WithUnits(pxl)}},
-             cam_rot::Rotation{3};
+             cam_rot::Union{Rotation{3}, Nothing};
              measurement_covariance::AbstractMatrix{<:Real} = I(2*length(measurements)), # defined as [x_1, y_1, x_2...]
              world_pts_lines::AbstractVector{<:NTuple{2, <:XYZ}} = NTuple{2, XYZ{typeof(1.0m)}}[],
              measured_lines::AbstractVector{<:Tuple} = Tuple{typeof(1.0pxl), typeof(1.0m)}[], #[(0.004489402014602179 * pxl, -1.8384693995160628 * rad)],
              lines_covariance::AbstractMatrix{<:Real} = I(2*length(measured_lines)),  # defined as [rho_1, theta_1, rho_2, ...]
              runway_pts::AbstractArray{<:XYZ{<:WithUnits(m)}}=world_pts,
-             initial_guess::XYZ{<:WithUnits(m)} = XYZ(-3000.0m, 0m, 30m),
+             initial_guess_loc::XYZ{<:WithUnits(m)} = XYZ(-3000.0m, 0m, 30m),
+             initial_guess_rot::AbstractVector{<:Real} = [0.0, 0.0, 0.0], # roll, pitch, yaw (radians)
              components=[:x, :y],
              solver=SimpleNewtonRaphson(), #NonlinearSolve.LevenbergMarquardt() #SimpleNewtonRaphson()
              )
     isempty(components) && return initial_guess
     N = length(world_pts); @assert N == length(measurements)
 
-    loss_no_α(loc, (; cam_rot, world_pts, measurements, measured_lines, runway_pts)) = begin
-        simulated_projections = project.([CamTransform(cam_rot, XYZ(loc*m))], world_pts)
-        results = []
-        # process points
-        if (:x in components || :y in components)
-            inv_weights = cholesky(measurement_covariance).U'
-            res_pts = let measurements_ = SVector(flatten(measurements)...),
-                        simulated_projections_ = SVector(flatten(simulated_projections)...)
-                res = measurements_ .- simulated_projections_
-                inv_weights \ ustrip.(pxl, res)
-            end
-            append!(results, res_pts)
-        end
-
-        T = promote_type(eltype.(results)...)
-        return reduce(vcat, results; init=T[])
-    end
-
-    loss(loc, (; cam_rot, world_pts, measurements, measured_lines, runway_pts)) = begin
+    loss(pose, (; cam_rot, world_pts, measurements, measured_lines, runway_pts, use_lines)) = begin
+        loc = pose[1:3]
+        cam_rot = isnothing(cam_rot) ? RotXYZ(roll=pose[4], pitch=pose[5], yaw=pose[6]) : cam_rot
         simulated_projections = project.([CamTransform(cam_rot, XYZ(loc*m))], world_pts)
         results = []
         # process points
@@ -52,7 +37,7 @@ function pnp(world_pts::AbstractVector{<:XYZ{<:WithUnits(m)}},
         end
 
         # process lines
-        if (:α in components)
+        if use_lines
             # Convert measurement covariance to line projection (add extra elements)
             cov_ext = extend_covariance(lines_covariance)
             inv_weights = cholesky(cov_ext).U'
@@ -75,122 +60,28 @@ function pnp(world_pts::AbstractVector{<:XYZ{<:WithUnits(m)}},
     end
 
     # Run first without edges to get good initial guess
-    initial_guess = MVector{3}(ustrip.(m, initial_guess))
-    ps = (; cam_rot, world_pts, measurements, measured_lines, runway_pts)
-    prob = NonlinearLeastSquaresProblem(loss_no_α, initial_guess, ps)
+    if isnothing(cam_rot)
+        initial_guess = MVector{6}(vcat(ustrip.(m, initial_guess_loc), initial_guess_rot))
+    else
+        initial_guess = MVector{3}(ustrip.(m, initial_guess_loc))
+    end
+    ps = (; cam_rot, world_pts, measurements, measured_lines, runway_pts, use_lines=false)
+    prob = NonlinearLeastSquaresProblem(loss, initial_guess, ps)
     res = solve(prob, solver; maxiters=1_000, abstol=1e-2)
     @assert res.retcode == ReturnCode.Success res
     if !(:α in components)
-        return XYZ(res.u*m)
+        return isnothing(cam_rot) ? (XYZ(res.u[1:3]*m), res.u[4:6]) : XYZ(res.u*m)
     end
 
     # Run with edges with good initial guess
-    new_initial_guess = XYZ(res.u*m)
-    new_initial_guess = MVector{3}(ustrip.(m, new_initial_guess))
+    new_initial_guess = isnothing(cam_rot) ? MVector{6}(res.u) : MVector{3}(res.u)
+    # println(new_initial_guess)
+    ps = (; cam_rot, world_pts, measurements, measured_lines, runway_pts, use_lines=true)
     prob = NonlinearLeastSquaresProblem(loss, new_initial_guess, ps)
     res = solve(prob, solver; maxiters=1_000, abstol=1e-2)
     @assert res.retcode == ReturnCode.Success res
-    return XYZ(res.u*m)
-end
-
-function pnp(world_pts::AbstractVector{<:XYZ{<:WithUnits(m)}},
-    measurements::AbstractVector{<:ImgProj{<:WithUnits(pxl)}};
-    measurement_covariance::AbstractMatrix{<:Real} = I(2*length(measurements)), # defined as [x_1, y_1, x_2...]
-    world_pts_lines::AbstractVector{<:NTuple{2, <:XYZ}} = NTuple{2, XYZ{typeof(1.0m)}}[],
-    measured_lines::AbstractVector{<:Tuple} = Tuple{typeof(1.0pxl), typeof(1.0m)}[], #[(0.004489402014602179 * pxl, -1.8384693995160628 * rad)],
-    lines_covariance::AbstractMatrix{<:Real} = I(2*length(measured_lines)),  # defined as [rho_1, theta_1, rho_2, ...]
-    runway_pts::AbstractArray{<:XYZ{<:WithUnits(m)}}=world_pts,
-    initial_guess_loc::XYZ{<:WithUnits(m)} = XYZ(-3000.0m, 0m, 30m),
-    initial_guess_rot::AbstractVector{<:Real} = [0.0, 0.0, 0.0], # roll, pitch, yaw (radians)
-    components=[:x, :y],
-    solver=SimpleNewtonRaphson(),
-    )
-    isempty(components) && return initial_guess
-    N = length(world_pts); @assert N == length(measurements)
-
-    loss_no_α(pose, (; world_pts, measurements, measured_lines, runway_pts)) = begin
-        loc = pose[1:3]
-        cam_rot = RotXYZ(roll=pose[4], pitch=pose[5], yaw=pose[6])
-        simulated_projections = project.([CamTransform(cam_rot, XYZ(loc*m))], world_pts)
-        results = []
-        # process points
-        if (:x in components || :y in components)
-            inv_weights = cholesky(measurement_covariance).U'
-            res_pts = let measurements_ = SVector(flatten(measurements)...),
-                        simulated_projections_ = SVector(flatten(simulated_projections)...)
-                res = measurements_ .- simulated_projections_
-                inv_weights \ ustrip.(pxl, res)
-            end
-            append!(results, res_pts)
-        end
-
-        T = promote_type(eltype.(results)...)
-        return reduce(vcat, results; init=T[])
-    end
-
-    loss(pose, (; world_pts, measurements, measured_lines, runway_pts)) = begin
-        loc = pose[1:3]
-        cam_rot = RotXYZ(roll=pose[4], pitch=pose[5], yaw=pose[6])
-        simulated_projections = project.([CamTransform(cam_rot, XYZ(loc*m))], world_pts)
-        results = []
-        # process points
-        if (:x in components || :y in components)
-            inv_weights = cholesky(measurement_covariance).U'
-            res_pts = let measurements_ = SVector(flatten(measurements)...),
-                        simulated_projections_ = SVector(flatten(simulated_projections)...)
-                res = measurements_ .- simulated_projections_
-                inv_weights \ ustrip.(pxl, res)
-            end
-            append!(results, res_pts)
-        end
-
-        # process lines
-        if (:α in components)
-            # Convert measurement covariance to line projection (add extra elements)
-            cov_ext = extend_covariance(lines_covariance)
-            inv_weights = cholesky(cov_ext).U'
-            # Project lines
-            simulated_lines = project_line.([CamTransform(cam_rot, XYZ(loc*m))], [ImgProj(0 * pxl, 0 * pxl)], world_pts_lines)
-            p = project_line_onto_unit_circle
-            res_lines = let 
-                measurements_ = SVector(flatten(p.(measured_lines))...)
-                simulated_lines_ = SVector(flatten(p.(simulated_lines))...)
-                res = (measurements_ - simulated_lines_)  # .* repeat([0;1;1], 2)
-                inv_weights \ res
-            end
-            for elem in res_lines
-                append!(results, elem)
-            end
-        end
-
-        T = promote_type(eltype.(results)...)
-        return reduce(vcat, results; init=T[])
-    end
-
-    # Run first without edges to get good initial guess
-    initial_guess = MVector{6}(vcat(ustrip.(m, initial_guess_loc), initial_guess_rot))
-    ps = (; world_pts, measurements, measured_lines, runway_pts)
-    prob = NonlinearLeastSquaresProblem(loss_no_α, initial_guess, ps)
-    maxiters = (:α in components ? 10_000 : 1_000)
-    res = solve(prob, solver; maxiters, abstol=1e-2)
-    @assert res.retcode == ReturnCode.Success "$((res, res.retcode))"
-    # @info loss(copy(res.u), ps)
-    # @info typeof(res)
-    loc = XYZ(res.u[1:3]*m)
-    rot = res.u[4:6]
-    # @info loss(vcat(ustrip.(m, loc), rot), ps)
-    if !(:α in components)
-        return loc, rot
-    end
-
-    # Run with edges with good initial guess
-    new_initial_guess = vcat(ustrip.(m, loc), rot)
-    prob = NonlinearLeastSquaresProblem(loss, new_initial_guess, ps)
-    res = solve(prob, solver; maxiters=1_000, abstol=1e-2)
-    @assert res.retcode == ReturnCode.Success "$((res, res.retcode))"
-    loc = XYZ(res.u[1:3]*m)
-    rot = res.u[4:6]
-    return loc, rot
+    # println(res)
+    return isnothing(cam_rot) ? (XYZ(res.u[1:3]*m), res.u[4:6]) : XYZ(res.u*m)
 end
 
 project_angle_onto_unit_circle(angle::AngularQuantity) = Point2(cos(angle), sin(angle))
